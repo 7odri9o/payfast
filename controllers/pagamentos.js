40d9f46680fa71d4231fd0f3403cb17d463ad6fa
Rodrigo Rocha <rodrigo.rocha@concrete.com.br>
2018-04-08 01:49:03 -0300

    Setando o status e a data de pagamento

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -6,7 +6,10 @@ module.exports = function (app) {
 
     app.post('/pagamentos/pagamento', function (req, res) {
         var pagamento = req.body;
-        console.log(pagamento)
-        res.send('OK.');
+        console.log('Processando a Requisição de um novo pagamento')
+
+        pagamento.status = 'CRIADO';
+        pagamento.data = new Date();
+        res.send(pagamento);
     });
 }
